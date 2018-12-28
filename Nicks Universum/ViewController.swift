b90544460937d6bb59e640482fd4466cd6b96784
black.dragon74 <nickk.2974@gmail.com>
2018-12-29 00:00:31 +0530

    Initial source code
    Signed-off-by: black.dragon74 <nickk.2974@gmail.com>

---
+++ Nicks Universum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nicks Universum
+//
+//  Created by Nick on 12/28/18.
+//  Copyright © 2018 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
